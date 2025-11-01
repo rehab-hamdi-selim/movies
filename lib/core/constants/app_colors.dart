ed151179c2dd50f8dc7e3d5d60f885459be0bcca
Rehab Hamdy <209379697+rehab-hamdi-selim@users.noreply.github.com>
2025-11-01 20:10:19 +0200

    core/ app colors

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color lightScaffold = Colors.white;
+  static const Color lightAppBar = Colors.blue;
+  static const Color lightMovieCard = Color(0xfff7f2fa);
+  static const Color lightMainText = Colors.black;
+  static const Color lightSideText = Colors.grey;
+
+  static const Color darkScaffold = Color(0xFF0B0B0B);
+  static const Color darkAppBar = Color(0xFF232227);
+  static const Color darkMovieCard = Color(0xFF231F23);
+  static const Color darkMainText = Colors.white;
+  static const Color darkSideText = Color(0xFFBFB7BF);
+
+  static const Color accent = Color(0xFF08BAC6);
+  static const Color star = Color(0xFFFFC107);
+}
