7d674a0561fe057126ea4d0dee5a27646cfcc92f
Rehab Hamdy <209379697+rehab-hamdi-selim@users.noreply.github.com>
2025-11-01 20:13:02 +0200

    data\ movie model

---
+++ lib/features/movies/data/models/movie_model.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'movie_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+MovieModel _$MovieModelFromJson(Map<String, dynamic> json) => MovieModel(
+  id: (json['id'] as num).toInt(),
+  title: json['title'] as String,
+  overview: json['overview'] as String,
+  posterPath: json['poster_path'] as String?,
+  voteAverage: (json['vote_average'] as num).toDouble(),
+);
+
+Map<String, dynamic> _$MovieModelToJson(MovieModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'overview': instance.overview,
+      'poster_path': instance.posterPath,
+      'vote_average': instance.voteAverage,
+    };
